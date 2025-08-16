a60c257c95c2db820368f32a57c88c542ce05df8
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2025-08-16 21:01:53 +0200

    Fix: iOS 18 openURL(_:) is deprecated

--- src/ios/Launcher.m
+++ src/ios/Launcher.m
@@ -1,3 +1,4 @@
+
 #import "Launcher.h"
 #import <Cordova/CDV.h>
 
@@ -30,7 +31,11 @@
 		NSString *uri = [options objectForKey:@"uri"];
 		if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:uri]]) {
 			NSURL *launchURL = [NSURL URLWithString:uri];
-			[[UIApplication sharedApplication] openURL: launchURL];
+			if (@available(iOS 10.0, *)) {
+				[[UIApplication sharedApplication] openURL:launchURL options:@{} completionHandler:nil];
+			} else {
+				[[UIApplication sharedApplication] openURL: launchURL];
+			}
 			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 		} else {
